d36c8d819dbc8d6bc15087a99d6431e770bd32f8
bemconcreto <bemconcretoni@gmail.com>
2025-11-05 18:22:36 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true, // garante compatibilidade com page.tsx e layout.tsx
-  },
 };
 
 module.exports = nextConfig;
